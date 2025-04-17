836ff7f2b6bda567eb4d0dab84a32c0e9aae55f9
bruce-mig <brucetmig@icloud.com>
2025-04-17 10:38:24 +0200

    unused test

--- product-command-service/src/test/java/com/github/bruce_mig/cqrs/product_command_service/ProductCommandServiceApplicationTests.java
+++ product-command-service/src/test/java/com/github/bruce_mig/cqrs/product_command_service/ProductCommandServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.bruce_mig.cqrs.product_command_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProductCommandServiceApplicationTests {
 
 	@Test
